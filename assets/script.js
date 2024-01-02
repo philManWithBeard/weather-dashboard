a0fd54f917e98f2dff7c7561ea68c65da92bfa03
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-01-02 14:22:46 +0000

    API key

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,6 @@ const searchButtonEl = $("#search-button");
 const historyEl = $("#history");
 const todayEl = $("#today");
 const forecastEl = $("#forecast");
+
+// API key
+const apiKey = "4ced624ce41090f360874722c5d79801";

e68f0bf353aa6f6a74f8d91652e5506454d7d3c0
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-01-02 14:23:22 +0000

    Handler for set data

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,26 @@ const forecastEl = $("#forecast");
 
 // API key
 const apiKey = "4ced624ce41090f360874722c5d79801";
+
+// Handler for set data
+let setData = (city) => {
+  let data = getData(); // call getdata handler for getting data from localstorage
+  let change = 0;
+
+  // Look through existing data
+  data.forEach((existingCity, i) => {
+    // See if there's already an entry for this city
+    if (existingCity == city) {
+      change++;
+    }
+  });
+
+  // If there's no existing entry then create one
+  if (change === 0) {
+    data.push(city);
+  }
+
+  // Convert to JSON and store data in localstorage
+  data = JSON.stringify(data);
+  localStorage.setItem("cityList", data);
+};

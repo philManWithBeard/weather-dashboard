be30a4114e5130d47dd84b2884adaa170483da74
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-01-02 14:22:23 +0000

    References to important DOM elements

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// References to important DOM elements
+const formHeadingEl = $("#form-heading");
+const searchFormEl = $("#search-form");
+const searchInputEl = $("#search-input");
+const searchButtonEl = $("#search-button");
+const historyEl = $("#history");
+const todayEl = $("#today");
+const forecastEl = $("#forecast");
